0a3f38e68d342c55d7ca0286521db6db9e1e205d
Claudio Narvaez <cnarvaez@integratepie.cl>
2025-05-12 11:35:10 -0400

    Se agrega json como contentype esperado

--- app/Http/Controllers/Api/V1/BeneficiosController.php
+++ app/Http/Controllers/Api/V1/BeneficiosController.php
@@ -76,16 +76,20 @@ class BeneficiosController extends Controller
     */
     public function get_beneficios_anuales(Request $request)
     {
-        $data = $this->apiService->getBeneficiosMergeData();
+        if($request->expectsJson()){
+            $data = $this->apiService->getBeneficiosMergeData();
 
-        $beneficiosFiltered = $this->collectionHelper->filterByAmountRange($data);
-        $beneficiosYear = $this->collectionHelper->groupByYear($beneficiosFiltered);
-        $beneficiosMapped = $this->collectionHelper->mapGroupData($beneficiosYear);
+            $beneficiosFiltered = $this->collectionHelper->filterByAmountRange($data);
+            $beneficiosYear = $this->collectionHelper->groupByYear($beneficiosFiltered);
+            $beneficiosMapped = $this->collectionHelper->mapGroupData($beneficiosYear);
 
-        return response()->json([
-            'code' => 200,
-            'success' => true,
-            'data' => $beneficiosMapped
-        ]);
+            return response()->json([
+                'code' => 200,
+                'success' => true,
+                'data' => $beneficiosMapped
+            ]);
+        }
+
+        return response('Request expected a JSON response', 406);
     }
 }
